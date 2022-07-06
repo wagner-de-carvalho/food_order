2aeb4e70b325d40b06eb54e4a13b80fc664ca117
wagner <wagncarv@outlook.com>
2022-07-06 15:57:56 -0300

    Refatoração formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 [
   import_deps: [:ecto, :phoenix],
-  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  inputs: ["*.{ex,exs,heex}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs,heex}"],
   subdirectories: ["priv/*/migrations"]
 ]
