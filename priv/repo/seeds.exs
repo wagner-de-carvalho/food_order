81ececb9f3853d9e6284b26d5b0e64e97276cfc5
wagner-de-carvalho <wagncarv@outlook.com>
2022-07-16 13:19:36 -0300

    configurações

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,3 +1,4 @@
+import FoodOrder.Factory
 alias FoodOrder.Accounts
 alias FoodOrder.Factory
 alias FoodOrder.Products
@@ -7,4 +8,4 @@ user_admin = %{email: "admin@gmail.com", password: "W_elixirpro17", role: "ADMIN
 Accounts.register_user(user)
 Accounts.register_user(user_admin)
 
-# Enum.each(1..200, fn _ -> insert(:product) end)
+ Enum.each(1..200, fn _ -> insert(:product) end)
