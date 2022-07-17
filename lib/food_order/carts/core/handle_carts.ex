ce2212223cda250246377384e3da66a10b1d8208
wagner-de-carvalho <wagncarv@outlook.com>
2022-07-17 17:21:50 -0300

    Função add_new_item criada

--- lib/food_order/carts/core/handle_carts.ex
+++ lib/food_order/carts/core/handle_carts.ex
@@ -2,4 +2,17 @@ defmodule FoodOrder.Carts.Core.HandleCarts do
   alias FoodOrder.Carts.Data.Cart
 
   def create_carts(id), do: Cart.new(id)
+
+  def add_new_product(cart, item) do
+    new_total_price = Money.add(cart.total_price, item.price)
+    new_total_items = cart.total_items + 1
+
+    %{
+      cart
+      | total_quantity: cart.total_quantity + 1,
+        items: cart.items ++ [item],
+        total_price: new_total_price,
+        total_items: new_total_items
+    }
+  end
 end
