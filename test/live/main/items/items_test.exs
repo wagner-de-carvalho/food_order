631a7afb5c3e4c1c2b4bc11664f1ed7a2c550df6
wagner-de-carvalho <wagncarv@outlook.com>
2022-07-17 00:09:51 -0300

    Testes hooks

--- test/live/main/items/items_test.exs
+++ test/live/main/items/items_test.exs
@@ -1,10 +1,38 @@
 defmodule FoodOrderWeb.Main.ItemsTest do
   use FoodOrderWeb.ConnCase
   import Phoenix.LiveViewTest
+  import FoodOrder.Factory
 
   test "load items", %{conn: conn} do
     {:ok, view, _html} = live(conn, Routes.main_path(conn, :index))
     assert has_element?(view, "#items-component")
     assert has_element?(view, "[data-role=items-info][data-id=all-foods]", "All Foods")
+  end
+
+  test "Should load more elements", %{conn: conn} do
+    products = for _ <- 0..12, do: insert(:product)
+    {:ok, view, _html} = live(conn, Routes.main_path(conn, :index))
+
+    open_browser(view)
+
+    [products_page_1, products_page_2] =
+      products
+      |> Enum.chunk_every(8)
+
+    Enum.each(products_page_1, fn product ->
+      assert has_element?(view, "#item-#{product.id}")
+    end)
+
+    Enum.each(products_page_2, fn product ->
+      refute has_element?(view, "#item-#{product.id}")
+    end)
+
+    view
+    |> element("#products-loading")
+    |> render_hook("load-more", %{})
+
+    Enum.each(products_page_2, fn product ->
+      assert has_element?(view, "#item-#{product.id}")
+    end)
   end
 end
