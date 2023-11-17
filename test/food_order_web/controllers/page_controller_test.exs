d894937eebbb7010954657b3448d38a5d4c586a8
mindlab <wagncarv@outlook.com>
2023-11-17 18:37:37 -0300

    Product page, tailwind formatting

--- test/food_order_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule FoodOrderWeb.PageControllerTest do
-  use FoodOrderWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Order"
-  end
-end
